3a909363f74ff411b6828c04b74ef861eb524994
Daan De Deckere <daandedeckere@gmail.com>
2019-01-16 15:27:38 +0100

    Add next.js

--- server/index.js
+++ server/index.js
@@ -1,23 +1,36 @@
 const express = require('express');
+const next = require('next');
 const redis = require('redis');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-const client = redis.createClient({ host: 'redis' });
+const dev = process.env.NODE_ENV !== 'production';;
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
-client.on("error", function (err) {
+const redisClient = redis.createClient({ host: 'redis' });
+
+redisClient.on("error", function (err) {
   console.log("Error " + err);
 });
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  client.incr('counter', function(err, reply) {
-    res.send('Hello, world\n' + reply);
-  });
-});
+nextApp.prepare()
+  .then(() => {
+    const server = express();
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+    server.get('/redis', (req, res) => {
+      redisClient.incr('counter', function(err, reply) {
+        res.send('Hello, world\n' + reply);
+      });
+    });
+
+    server.get('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(PORT, HOST);
+    console.log(`Running on http://${HOST}:${PORT}`);
+
+  })
