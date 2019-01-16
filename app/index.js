c3944cddd9f8e2cd0d19643152406b9aab004f9b
Daan De Deckere <daandedeckere@gmail.com>
2019-01-16 14:53:11 +0100

    Add Redis with demo counter

--- app/index.js
+++ app/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const redis = require('redis');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const client = redis.createClient({ host: 'redis' });
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  client.incr('counter', function(err, reply) {
+    res.send('Hello, world\n' + reply);
+  });
 });
 
 app.listen(PORT, HOST);
